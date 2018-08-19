062b0ec44f11509989e83c93b8d1da2176195134
naveenramya <41774158+naveenramya@users.noreply.github.com>
2018-08-19 16:36:47 +0530

    Create swap2numbers.java

---
+++ swap2numbers.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	            int a,b,c;
+		Scanner s=new Scanner(System.in);
+		a=s.nextInt();
+		b=s.nextInt();
+		c=a;
+		a=b;
+		b=c;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}

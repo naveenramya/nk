e1417e3856c6f35b1fbff7a53faa0563823845f4
naveenramya <41774158+naveenramya@users.noreply.github.com>
2018-08-22 16:13:26 +0530

    Create fibonacciseries.java

---
+++ fibonacciseries.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n1=0,n2=1,n3,i,count=5;    
+                        System.out.print(n2);
+                        for(i=1;i<count;i++)   
+		{    
+			n3=n1+n2;    
+			System.out.print(" "+n3);    
+			n1=n2;    
+			n2=n3;    
+		}    
+	}
+}
+		
+		

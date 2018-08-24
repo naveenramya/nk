108c9a22b4ec8838bcba89a672d190d9b7de5db1
naveenramya <41774158+naveenramya@users.noreply.github.com>
2018-08-24 10:36:53 +0530

    Create 44.java

---
+++ 44.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		if(n==1 || n==2 || n==3 || n==4 || n==5 || n==6 || n==7 ||n==8 || n==9 ||n==10)
+		{
+			System.out.println("yes");
+		}
+		else
+		{
+			System.out.println("no");
+		}
+	}
+}
+	
+
+		
+	
